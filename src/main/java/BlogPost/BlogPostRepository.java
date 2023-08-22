45f1350db35b9b71f2ebc71ad3d369efa9b87f2a
katherinemoore <katherinemoore789@gmail.com>
2023-08-21 18:07:16 -0700

    Upload files from local repo

---
+++ src/main/java/BlogPost/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package BlogPost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+}
---
+++ src/test/java/com/tts/BlogWebsite/BlogWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.BlogWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
